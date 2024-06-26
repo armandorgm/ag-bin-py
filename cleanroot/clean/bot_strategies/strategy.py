e906a089d45efc14aef7e4320ae18fbf49b41b30
Armando <armandorgm3@gmail.com>
2024-06-26 09:08:38 -0300

    created test strategy

--- cleanroot/clean/bot_strategies/strategy.py
+++ cleanroot/clean/bot_strategies/strategy.py
@@ -1,6 +1,10 @@
-from decimal import Decimal
+from decimal import ROUND_UP, Decimal
+import json
+from pprint import pprint
 from typing import Any, Awaitable, Callable, Coroutine, List
 from abc import ABC, abstractmethod
+
+from ..interfaces.strategy_interface import StrategyImplementor
 
 from ..interfaces.types import OrderType
 from ..interfaces.exchange_basic import SymbolPrecision,Order,PositionSide,OrderSide
@@ -17,15 +21,30 @@ class Strategy(ABC):
     def do_algorithm(self, data: List):
         pass
     """
+    def __init__(self,interface:StrategyImplementor) -> None:
+        self.interface = interface
+        
     @abstractmethod
     def saveState(self):
         pass
     
     @abstractmethod
-    def evaluar_precio(self, datos_mercado:Decimal, orden_callback: Callable[[PositionSide, OrderSide, Decimal, Decimal,OrderType], Coroutine[Any,Any,Order]])->Coroutine[Any,Any,Order]:
+    def evaluar_precio(self, datos_mercado:Decimal)->Any:
         pass
     
     @staticmethod
-    @abstractmethod
-    def parseStrategyData(stringSerializedStrategyData:str)->Any:
-        pass
+    def from_json(json_string:str):
+        print("Strategy.from_json:")
+        datos = json.loads(json_string)
+        pprint(datos)
+        return datos
+    
+    
+    def get_min_amount(self,price:Decimal)->Decimal:
+        rawAmount = (self.interface.notionalMin/price)
+        print("rawAmount",rawAmount)
+        print(self.interface.amountPrecision)
+        formatedAmount = rawAmount.quantize(Decimal("1e-{0}".format(self.interface.amountPrecision)),rounding=ROUND_UP)
+        print("formatedAmount",formatedAmount)
+        return formatedAmount
+    
---
+++ test/test_strategy.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from typing import Any
+from unittest import TestCase
+from unittest.mock import MagicMock
+from cleanroot.clean.bot_strategies.strategy import Strategy
+
+class FooStrategy(Strategy):
+    def saveState(self):
+        pass
+    def evaluar_precio(self, datos_mercado: Decimal) -> Any:
+        pass
+    
+class Test_Strategy(TestCase):
+    def setUp(self):
+        self.mock_interface = MagicMock()
+        self.strategy = FooStrategy(self.mock_interface)
+    
+    def test_get_min_amount(self,):
+        self.mock_interface.amountPrecision = 8
+        self.mock_interface.notionalMin = 33555
+        
+        price = Decimal(100)
+        self.assertEqual(float(self.strategy.get_min_amount(price)),335.55)
+        
+    def test_get_min_amount2(self,):
+        self.mock_interface.amountPrecision = 1
+        self.mock_interface.notionalMin = 33555
+        
+        price = Decimal(100)
+        self.assertEqual(float(self.strategy.get_min_amount(price)),335.56)
+    
